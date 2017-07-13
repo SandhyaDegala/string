62e62b499256f39697f321ac6f5e28a3f0424325
sandhyad <sandhya.degala@alacriti.com>
2017-07-13 16:57:41 +0530

    some files are added

---
+++ demo/src/main/java/com/alacriti/App.java
@@ -0,0 +1,14 @@
+package com.alacriti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        StringManipulation s=new StringManipulation();
+        s.index();
+    }
+}
